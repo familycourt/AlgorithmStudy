88c1eeb1093c96836cfc1d5a316d638af1f6e891
ssongjay <jay5326@naver.com>
2022-07-22 23:16:02 +0900

    소수 구하기 / 40m

--- week1/ssong_b1929.py
+++ week1/ssong_b1929.py
@@ -1,17 +1,19 @@
 # 시작 시간 22-07-22 22:25
 
 import sys
-
-from sympy import false, true
+import math
 
 M, N = map(int, sys.stdin.readline().split())
 
-def is_prime_num(num):
-    for i in range(2, num):
-        if num % i == 0:
-            return false
-        return true
+b = 0
 
 for i in range(M, N):
-    if is_prime_num(i) == true:
+    for j in range(2, int(math.sqrt(i) + 1)):
+        #print(num, i)
+        if i % j == 0:
+            b = 1
+    
+    if b == 0:
         print(i)
+    b = 0
+
