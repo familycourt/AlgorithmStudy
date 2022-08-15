09bbfa31f4a44a44d790d8bcf3976010b603d305
James <jamesj0107@naver.com>
2022-08-16 02:01:17 +0900

    소인수분해 / 15m

--- week1/james_b9020.py
+++ week1/james_b9020.py
@@ -1,23 +1,12 @@
 from sys import stdin
 
-
-def is_prime(n):
-    if n == 1:
-        return False
-    i = 2
-    while i * i <= n:
-        if n % i == 0:
-            return False
-        i += 1
-    return True
-
-
 n = int(stdin.readline().strip())
 
-for _ in range(n):
-    x = int(stdin.readline().strip())
-
-    for i in range(int(x / 2), x):
-        if is_prime(i) and is_prime(x - i):
-            print(x - i, i)
-            break
+factor = 2
+while n != 1:
+    if n % factor == 0:
+        print(factor)
+        n = int(n / factor)
+        factor = 2
+    else:
+        factor += 1
