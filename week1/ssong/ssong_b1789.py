3a91b61bd5bf7a8ffd14bc676c0ad4edeecd6cbc
ssongjay <jay5326@naver.com>
2022-09-05 01:56:56 +0900

    수들의 합 / 1h 20m

--- week1/ssong/ssong_b1789.py
+++ week1/ssong/ssong_b1789.py
@@ -6,12 +6,17 @@ import sys
 S = int(sys.stdin.readline())
 
 # 더하는 갯수가 가장 많을 때를 구해라
+# TODO: 빼고 난 결과가 다음 수보다 작다면 빼지 않고 다음 수로 넘어감
 cnt = 0
-for i in range(S):
-    S = S - (i + 1)
-    if(S < i):
+for i in range(1, S + 1):
+    if(S == i):
+        cnt += 1
         break
-    print(S, i + 1)
+    if ((S - i) < i + 1):
+        continue
+    S = S - i
     cnt += 1
-cnt -= 1
+
+
+# 뺀 다음수보다 S가 작다면 다음수가 마지막 수이다
 print(cnt)
