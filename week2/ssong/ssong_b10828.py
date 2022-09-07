d6a2044e0d85767c244ae970687db80ee2e3c577
ssongjay <jay5326@naver.com>
2022-09-07 19:35:34 +0900

    스택 / 32m

--- week2/ssong/ssong_b10828.py
+++ week2/ssong/ssong_b10828.py
@@ -6,23 +6,28 @@ calculate_number = int(sys.stdin.readline())
 
 stack = []
 
+
 def is_empty(stack):
     if len(stack) == 0:
         return True
     else:
         return False
 
+
 def get_size(stack):
     print(len(stack))
+
 
 def push(stack, num):
     stack.append(num)
 
+
 def pop(stack):
-    if is_empty(stack): # 스택 비어있다면
+    if is_empty(stack):  # 스택 비어있다면
         print("-1")
     else:
         print(stack.pop())
+
 
 def is_empty_stack(stack):
     if is_empty(stack):
@@ -30,11 +35,13 @@ def is_empty_stack(stack):
     else:
         print('0')
 
+
 def get_top(stack):
-    if is_empty(stack): # 스택 비어있다면
+    if is_empty(stack):  # 스택 비어있다면
         print('-1')
     else:
         print(stack[-1])
+
 
 for _ in range(calculate_number):
     menu = [x for x in sys.stdin.readline().split()]
