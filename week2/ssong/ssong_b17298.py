aa904e8082f2eef2335cb02ebc8638907443c061
ssongjay <jay5326@naver.com>
2022-09-20 15:32:28 +0900

    오큰수 / 42m

---
+++ week2/ssong/ssong_b17298.py
@@ -0,0 +1,22 @@
+# 오큰수
+
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+
+A = [int(x) for x in sys.stdin.readline().rstrip().split()]
+
+stack = []
+answer = deque()
+for i in range(N - 1, -1, -1):
+    while stack:
+        if stack[-1] > A[i]:
+            answer.appendleft(stack[-1])
+            break
+        else:
+            stack.pop()
+    if not stack:
+        answer.appendleft(-1)
+    stack.append(A[i])
+print(' '.join(map(str, answer)))
