fb011d540e7446925f45d7c527e04f708f342591
James Jung <therealjamesjung@gmail.com>
2022-11-05 18:17:33 +0900

    숫자 문자열과 영단어 / 3m

---
+++ kakao/james/james_p81301.py
@@ -0,0 +1,16 @@
+def solution(s):
+    n = {
+        'zero': '0',
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9',
+    }
+    for k, v in n.items():
+        s = s.replace(k, v)
+    return int(s)
