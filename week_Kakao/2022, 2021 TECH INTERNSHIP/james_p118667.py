b0cbfe93160999d0fd2bf4f0dece8119b51aa976
James Jung <therealjamesjung@gmail.com>
2022-11-06 19:35:01 +0900

    두 큐 합 같게 만들기 / 1h 30m

--- week_Kakao/2022, 2021 TECH INTERNSHIP/james_p118667.py
+++ week_Kakao/2022, 2021 TECH INTERNSHIP/james_p118667.py
@@ -14,6 +14,7 @@ def solution(queue1, queue2):
     while True:
         if not queue1 or not queue2 or result > length * 2:
             return -1
+
         if s1 < s2:
             tmp = queue2.popleft()
             queue1.append(tmp)
