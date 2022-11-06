84d1c32dd30560b5eab8a0e5c602cd77bc4e52c5
James Jung <therealjamesjung@gmail.com>
2022-11-06 19:31:42 +0900

    두 큐 합 같게 만들기 / 1h 30m

---
+++ week_Kakao/2022 TECH INTERNSHIP/james_p118667.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+
+def solution(queue1, queue2):
+    s1, s2 = sum(queue1), sum(queue2)
+    queue1, queue2 = deque(queue1), deque(queue2)
+    length = len(queue1 + queue2)
+
+    if sum(queue1 + queue2) % 2 != 0:
+        return -1
+
+    result = 0
+
+    while True:
+        if not queue1 or not queue2 or result > length * 2:
+            return -1
+        if s1 < s2:
+            tmp = queue2.popleft()
+            queue1.append(tmp)
+            s2 -= tmp
+            s1 += tmp
+            result += 1
+        elif s1 > s2:
+            tmp = queue1.popleft()
+            queue2.append(tmp)
+            s2 += tmp
+            s1 -= tmp
+            result += 1
+        else:
+            return result
+
+
+print(solution([3, 2, 7, 2], [4, 6, 5, 1]))
